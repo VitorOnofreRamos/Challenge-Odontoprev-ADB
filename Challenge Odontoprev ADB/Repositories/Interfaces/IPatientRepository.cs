70d05f0f3666d236753a948f40c5186d720e8bac
Vitor Onofre Ramos <107143879+VitorOnofreRamos@users.noreply.github.com>
2024-10-02 20:46:22 -0300

    Remoção da Task GetPatientByIdAsync da interface IPatientRepository

--- Challenge Odontoprev ADB/Repositories/Interfaces/IPatientRepository.cs
+++ Challenge Odontoprev ADB/Repositories/Interfaces/IPatientRepository.cs
@@ -4,6 +4,5 @@ namespace Challenge_Odontoprev_ADB.Repositories.Interfaces;
 
 public interface IPatientRepository : _IRepository<Patient>
 {
-    Task<Patient> GetPatientByIdAsync(int id);
     Task<Patient> GetPatientByNameAsync(string name);
 }
